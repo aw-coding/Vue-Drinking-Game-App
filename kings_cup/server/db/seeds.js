f36588387e19546b5347ede296dd8e966ba4bcbb
Ewan Brain <ewan.brain@gmail.com>
2021-01-22 17:00:21 +0000

    created seeds

---
+++ kings_cup/server/db/seeds.js
@@ -0,0 +1,71 @@
+use game_rules;
+db.dropDatabase();
+
+db.default_rules.insertMany([
+    {
+        name: "Waterful",
+        text: "Rule",
+        value: "ACE"
+    },
+    {
+        name: "You",
+        text: "Rule",
+        value: "2"
+    },
+    {
+        name: "Me",
+        text: "Rule",
+        value: "3"
+    },
+    {
+        name: "Floor",
+        text: "Rule",
+        value: "4"
+    },
+    {
+        name: "Guys",
+        text: "Rule",
+        value: "5"
+    },
+    {
+        name: "Chicks",
+        text: "Rule",
+        value: "6"
+    },
+    {
+        name: "Heaven",
+        text: "Rule",
+        value: "7"
+    },
+    {
+        name: "Mate",
+        text: "Rule",
+        value: "8"
+    },
+    {
+        name: "Rhyme",
+        text: "Rule",
+        value: "9"
+    },
+    {
+        name: "Categories",
+        text: "Rule",
+        value: "10"
+    },
+    {
+        name: "Everyone Drinks",
+        text: "Rule",
+        value: "JACK"
+    },
+    {
+        name: "Questions",
+        text: "rule",
+        value: "QUEEN"
+    },
+    {
+        name: "King's Cup",
+        text: "rule",
+        value: "KING"
+    },
+
+])
