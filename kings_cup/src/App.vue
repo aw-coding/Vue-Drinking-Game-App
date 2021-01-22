d77caaf386f621067bf8d21cfd2e2765b3accfbf
Ewan Brain <ewan.brain@gmail.com>
2021-01-22 14:14:37 +0000

    first components created

--- kings_cup/src/App.vue
+++ kings_cup/src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
-    
+    <header-tile> </header-tile>
+    <game-tile></game-tile>
   </div>
 </template>
 
 <script>
-
+import HeaderTile from './components/HeaderTile.vue'
+import GameTile from './components/GameTile.vue'
 
 export default {
   name: 'App',
   components: {
+    'header-tile' : HeaderTile,
+    'game-tile' : GameTile
   
   }
 }
 </script>
+    
 
 <style>
 #app {
---
+++ kings_cup/src/components/HeaderTile.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Header Tile</h1>
+</template>
+
+<script>
+export default {
+    name: "header-tile"
+
+}
+</script>
+
+<style>
+
+</style>
