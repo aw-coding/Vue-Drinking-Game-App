dee0c04e84bdf4bebdd36bdbb800b18f98b31ce3
Ewan Brain <ewan.brain@gmail.com>
2021-01-22 14:42:03 +0000

    HeaderTile component added

---
+++ kings_cup/src/components/AddPlayerTile.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>New Player Form</h1>
+</template>
+
+<script>
+export default {
+    name: 'add-player-tile'
+
+}
+</script>
+
+<style>
+
+</style>
--- kings_cup/src/components/GameTile.vue
+++ kings_cup/src/components/GameTile.vue
@@ -1,8 +1,14 @@
 <template>
+    <div>
+        <add-player-tile></add-player-tile>
+
+    </div>
   
 </template>
 
 <script>
+import AddPlayerTile from '@/components/AddPlayerTile.vue'
+
 export default {
     name: 'game-tile',
     data () {
@@ -14,12 +20,20 @@ export default {
         getData: function () {
             fetch('https://deckofcardsapi.com/api/deck/new/draw/?count=52')
             .then(res => res.json())
-            .then(data => this.deck = data)
+            .then(data => this.deck = data.cards)
         }
     },
     mounted () {
         this.getData()
+        
+    },
+    components: {
+        'add-player-tile': AddPlayerTile
     }
+        
+    
+
+    
 
 
 
