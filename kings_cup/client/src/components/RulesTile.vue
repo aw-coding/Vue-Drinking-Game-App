971b48936c283f4f3f02559203fd07ee04802284
Andrew Wood <andrewwood90@hotmail.com>
2021-01-26 13:20:09 +0000

    house rules inputs displayed

--- kings_cup/client/src/components/RulesTile.vue
+++ kings_cup/client/src/components/RulesTile.vue
@@ -1,7 +1,14 @@
 <template lang="html">
 <div id="rules-tile">
-    <ul>
-        <li v-for="(rule, index) in rules" >
+
+    <label for='ruleset'> Choose Regular or House Rules:</label>
+    <select v-model='rulesChoice' name='ruleset' id='ruleset'>
+        <option value="regular-rules">Regular</option>
+        <option value="house-rules">House</option>
+    </select>
+
+    <ul v-if="rulesChoice === 'regular-rules'">
+        <li v-for="(rule, index) in rules"> 
         <h3>{{rule.value}}</h3>     
         <h4>{{rule.name}}</h4>  
         <p>{{rule.text}}</p>    
@@ -9,8 +16,23 @@
         </li>
     </ul>
 
+
+
+    <ul v-if="rulesChoice === 'house-rules'">
+        <li v-for="(rule, index) in houseRules" >
+        <h3>{{rule.value}}</h3>
+        <h4>{{rule.name}}</h4>     
+        <input :value="rule.name" >
+        <p>{{rule.text}}</p>    
+        <textarea :value="rule.text" type="textbox" rows="4" cols="60"></textarea>  
+    
+        </li>
+    </ul> 
+
+
+
+
 </div>
-  
 </template>
 
 <script>
@@ -19,12 +41,15 @@ export default {
     data() {
         return {
             rules: null,
+            houseRules: null,
+            rulesChoice: "regular-rules",
         }
     },
     mounted() {
         fetch('http://localhost:3000/api/rules')
         .then(res => res.json())
         .then(rules => this.rules = rules)
+        .then(rules => this.houseRules = rules)
     }
 
 
