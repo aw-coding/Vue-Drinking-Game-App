c3f7d397f9f765ea97c3990f70d74323479f0b5e
Ewan Brain <ewan.brain@gmail.com>
2021-01-26 16:18:01 +0000

    image added, text repositioned

--- kings_cup/client/src/components/HeaderTile.vue
+++ kings_cup/client/src/components/HeaderTile.vue
@@ -1,8 +1,10 @@
 <template>
-<div id="header">
-  <!-- <img src="https://cdn.shopify.com/s/files/1/0398/3985/8845/files/kings_cup_logo.jpg?v=1591773683" alt=""> -->
-  <h1>Big Deck Energy Presents: King's Cup</h1>
-</div>
+  <div>
+    <div id="header">
+      <img src="https://image.shutterstock.com/image-vector/golden-crown-royal-king-cartoon-600w-126722057.jpg" alt="Jester"> 
+      <h1>Big Deck Energy Presents: King's Cup</h1>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -14,9 +16,26 @@ export default {
 
 <style>
 
-h1{
-  margin-left: 10px;
-  
+#header{
+  display: grid;
+  grid-template-columns: 20% 80;
+  grid-template-rows: 10% 90%;
+  background: rgba(0, 0, 0, 0.686);
+  margin: 5px;
+  font-family: 'MedievalSharp', cursive;
+  font-size: 26px;
+  text-align: center;
+  height: 230px;
+  padding: 20px;
+}
+
+#header > img{
+  border-radius: 50%;
+  border-width: 10px;
+  border-style:dashed;
+  border-color: rgb(255, 227, 67);
+  height: 200px;
+  width: 200px;
 }
 
 </style>
