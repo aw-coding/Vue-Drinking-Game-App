ed6e1e9eeb6cbbbb6e2e38abd93a4a146900ad55
Simon Charters <simon259@hotmail.co.uk>
2021-01-26 19:10:08 +0000

    background now displaying as transparent

--- kings_cup/client/src/components/HeaderTile.vue
+++ kings_cup/client/src/components/HeaderTile.vue
@@ -1,6 +1,5 @@
 <template>
 <div id="header">
-  <!-- <img src="https://cdn.shopify.com/s/files/1/0398/3985/8845/files/kings_cup_logo.jpg?v=1591773683" alt=""> -->
   <h1>Big Deck Energy Presents: King's Cup</h1>
 </div>
 </template>
@@ -18,5 +17,6 @@ h1{
   margin-left: 10px;
   
 }
+
 
 </style>
