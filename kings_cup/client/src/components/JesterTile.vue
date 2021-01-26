db69389c9bd99a59d9f40bcea6972f31b6270834
Ewan Brain <ewan.brain@gmail.com>
2021-01-26 11:07:56 +0000

    jester tile added, pulling from joke api

---
+++ kings_cup/client/src/components/JesterTile.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id='jester-container'>
+        <h2> JESTER TILE </h2>
+        <p>{{joke.setup}}</p>
+        <p>{{joke.delivery}}</p>
+    </div>
+</template>
+
+<script>
+import { eventBus } from '@/main.js'
+export default {
+    data() {
+        return {
+            joke: []
+        }
+    },
+    mounted(){
+
+        eventBus.$on('next-players-turn', playerTurn => {
+            fetch('https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart')
+            .then(res => res.json())
+            .then(joke => this.joke = joke);
+            
+            })
+    },
+    methods: {
+    }
+
+
+}
+
+</script>
+
+<style>
+    #jester-container{
+        background: purple;
+        height: 200px;
+    }
+
+
+
+</style>
