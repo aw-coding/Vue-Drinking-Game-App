e70c2cafcad0d3184ff97f0e5a8c6b493b800bff
Simon Charters <simon259@hotmail.co.uk>
2021-01-27 10:15:47 +0000

    merging with develop

--- kings_cup/client/src/components/HeaderTile.vue
+++ kings_cup/client/src/components/HeaderTile.vue
@@ -1,16 +1,10 @@
 <template>
-<<<<<<< HEAD
-<div id="header">
-  <h1>Big Deck Energy Presents: King's Cup</h1>
-</div>
-=======
   <div>
     <!-- <div id="header">
       <img src="https://image.shutterstock.com/image-vector/golden-crown-royal-king-cartoon-600w-126722057.jpg" alt="Jester"> 
       <h1>Big Deck Energy Presents: King's Cup</h1>
     </div> -->
   </div>
->>>>>>> ccd14dd03b4cd172d3df5758a4dbcaf52e58b96c
 </template>
 
 <script>
@@ -22,13 +16,4 @@ export default {
 
 <style>
 
-<<<<<<< HEAD
-h1{
-  margin-left: 10px;
-  
-}
-
-
-=======
->>>>>>> ccd14dd03b4cd172d3df5758a4dbcaf52e58b96c
 </style>
