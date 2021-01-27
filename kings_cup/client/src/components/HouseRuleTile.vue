3b8f60abfeb0d884522e051e6c209d1681627932
Andrew Wood <andrewwood90@hotmail.com>
2021-01-27 11:57:30 +0000

    houserules succssfully added to db

---
+++ kings_cup/client/src/components/HouseRuleTile.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+
+
+        <h3>{{rule.value}}</h3>     
+        <input v-model='rule.name'>  
+        <textarea v-model='rule.text' type="textbox" rows="4" cols="60"></textarea>
+        <button v-on:click='updateRule'>Change Rule</button>  
+
+</div>  
+</template>
+
+<script>
+import RuleService from '@/services/RuleService'
+
+export default {
+    name: 'house-rule-tile',
+
+    props: ['rule'],
+
+        methods: {
+        updateRule: function () {
+
+            const newRule = {
+                _id: this.rule._id,
+                name: this.rule.name,
+                text: this.rule.text,
+                value: this.rule.value
+            };
+            console.log(newRule)
+
+            RuleService.updateRule(newRule);
+
+
+        },
+
+    }
+}
+</script>
+
+<style>
+
+</style>
